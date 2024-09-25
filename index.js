b5dc5175c70878add3312bf5502a16f93c511d94
Daniel Hormos <daniel_danne32@hotmail.com>
2024-09-25 12:01:40 +0200

    Changes done so that localhost:3000 runs a webpage to display Hello fellow Developer

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-type', 'text/plain')
+    res.end('Hello fellow developer!')
+})
+
+const hostname = 'localhost'
+const port = 3000
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+})
