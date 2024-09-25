cea27d8aa2f0d39f2f809c7da647166d3a6dac9d
Daniel Hormos <daniel_danne32@hotmail.com>
2024-09-25 12:55:09 +0200

    Handling request of faviIcon

--- index.js
+++ index.js
@@ -2,12 +2,32 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    const content = fs.readFileSync('./static/index.html', 'utf-8')
+    console.log(`The URL for the request was '${req.url}'`)
+    console.log(`The Method for the request was '${req.method}'`)
 
+    if (req.url === '/favicon.ico') {
+        res.writeHead(204, { 'Content-Type': 'image/x-icon' })
+        res.end()
+        return
+    }
+
+    const fileName = fileNameOfUrl(req.url)
+    const content = fs.readFileSync(`./static/${fileName}`, 'utf-8')
+    
     res.statusCode = 200
     res.setHeader('Content-type', 'text/html')
     res.end(content)
 })
+
+const fileNameOfUrl = (url) => {
+    let fileName = ''
+    if(url.split('/')[1] === ''){
+        fileName = 'index.html'
+    } else {
+        fileName = url.split('/')[1]
+    }
+    return fileName
+}
 
 const hostname = 'localhost'
 const port = 3000
