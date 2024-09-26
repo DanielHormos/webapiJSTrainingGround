b01532cac0bcc942c28bb115d344cd9d4466732f
Daniel Hormos <daniel_danne32@hotmail.com>
2024-09-26 14:34:36 +0200

    added app.get function again that i removed now I can retrieve 1 developer or all of them

--- index.js
+++ index.js
@@ -16,15 +16,29 @@ const db = [
 
 app.use(express.json())
 
+app.get('/api/developers', (req, res) => {
+  res.json(db);
+});
+
 app.get('/api/developers/:id', (req, res) => {
     const dev = db.find(dev => dev.id == req.params.id)
     return dev ? res.json(dev) : res.status(404).end()
 });
 
 app.post('/api/developers/', (req, res) => {
-    console.log(req.body)
-    res.status(201).end()
-})
+    const newDeveloper = {
+      id: db.length + 1,
+      name: req.body.name,
+      email: req.body.email,
+    };
+  
+    db.push(newDeveloper);
+  
+    res
+      .status(201)
+      .setHeader('location', `/api/developers/${newDeveloper.id}`)
+      .json(newDeveloper);
+  });
 
 const port = 3000
 app.listen(port, () => {
