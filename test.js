270e52429cff0ce891addc6179e431bc414c1802
Daniel Hormos <daniel_danne32@hotmail.com>
2024-09-26 19:00:03 +0200

    added test file to start testing express

---
+++ test.js
@@ -0,0 +1,13 @@
+const assert = require('assert')
+const request = require('supertest')
+
+describe('developers API should have endpoints to', () => {
+    it('get all developers', () => {
+        // arrange
+
+        // act
+
+        // assert
+        assert.strictEqual(false, true)
+    })
+})
